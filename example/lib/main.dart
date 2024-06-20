437e74b5bff36a6122e9e902fae5eef5072f1816
kudakov <kudakov@inspector-cloud.com>
2024-06-20 23:14:43 +0300

    add submenu

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,6 @@ import 'package:full_screen_image/full_screen_image.dart';
 import 'dart:math';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-
 void main() {
   int res = graphics.libGraphInit();
   runApp(const MyApp());
@@ -57,7 +56,8 @@ class _MyAppState extends State<MyApp> {
   final GlobalKey _imageKey = GlobalKey();
   double _scaleWidth = 1;
   double _scaleHeight = 1;
-  
+  bool _isDrawing = false; // State variable to track drawing mode
+
   Future<void> _pickImage(ImageSource source) async {
     var appDir = (await getTemporaryDirectory()).path;
     new Directory(appDir).delete(recursive: true);
@@ -80,16 +80,14 @@ class _MyAppState extends State<MyApp> {
       double scale_img = imageWidth / screenWidth;
       print("scale img = ${scale_img}");
 
-      if(scale_img < 1.0) {
+      if (scale_img < 1.0) {
         scale_img == 1.0;
       }
 
       setState(() {
         _image = copiedImage;
-        _scaleWidth =  scale_img;
+        _scaleWidth = scale_img;
       });
-
-
     }
   }
 
@@ -99,8 +97,6 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  
-
   @override
   void dispose() {
     DefaultCacheManager().emptyCache();
@@ -109,14 +105,16 @@ class _MyAppState extends State<MyApp> {
 
   String _generateRandomName(int length) {
     final random = Random();
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const chars =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     return String.fromCharCodes(Iterable.generate(
         length, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
   }
 
   Future<void> _processImage() async {
     final tempDir = await getTemporaryDirectory();
-    final randomName = _generateRandomName(10); // Generate a 10 character random name
+    final randomName =
+        _generateRandomName(10); // Generate a 10 character random name
     final processedImagePath = '${tempDir.path}/$randomName.jpg';
     final processImage = await _image!.copy(processedImagePath);
 
@@ -131,19 +129,18 @@ class _MyAppState extends State<MyApp> {
       throw Exception('Image processing failed');
     }
     setState(() {
-       _image = processImage;
+      _image = processImage;
       _points.clear();
     });
 
     temp.delete();
-
   }
 
   ffi.Pointer<ffi.Float> _convertPointsToNative(List<Offset> points) {
     final pointer = calloc<ffi.Float>(points.length * 2);
     for (int i = 0; i < points.length; i++) {
-      pointer[i * 2] = points[i].dx * _scaleWidth ;
-      pointer[i * 2 + 1] = points[i].dy  * _scaleWidth;
+      pointer[i * 2] = points[i].dx * _scaleWidth;
+      pointer[i * 2 + 1] = points[i].dy * _scaleWidth;
     }
     return pointer;
   }
@@ -154,14 +151,41 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Image Processing with FFI'),
+          actions: [
+            IconButton(
+              icon: Icon(_isDrawing ? Icons.edit_off : Icons.edit),
+              onPressed: () {
+                setState(() {
+                  _isDrawing = !_isDrawing;
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: _processImage,
+            ),
+            IconButton(
+              onPressed: () => _pickImage(ImageSource.gallery),
+              icon: const Icon(Icons.photo),
+            ),
+            const SizedBox(height: 10),
+            IconButton(
+              onPressed: () => _pickImage(ImageSource.camera),
+              icon: const Icon(Icons.camera),
+            ),
+
+
+          ],
         ),
         body: Center(
           child: GestureDetector(
             onPanUpdate: (details) {
-              RenderBox renderBox = context.findRenderObject() as RenderBox;
-              Offset localPosition =
-                  renderBox.globalToLocal(details.localPosition);
-              _addPoint(localPosition);
+              if (_isDrawing) {
+                RenderBox renderBox = context.findRenderObject() as RenderBox;
+                Offset localPosition =
+                    renderBox.globalToLocal(details.localPosition);
+                _addPoint(localPosition);
+              }
             },
             child: Stack(
               children: [
@@ -177,24 +201,69 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () => _pickImage(ImageSource.gallery),
-              child: const Icon(Icons.photo),
-            ),
-            const SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () => _pickImage(ImageSource.camera),
-              child: const Icon(Icons.camera),
-            ),
-            const SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () => _processImage(),
-              child: const Icon(Icons.edit),
-            ),
-          ],
+        floatingActionButton: SizedBox(
+          width: 56, // Standard size of a FloatingActionButton
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              DropdownMenuItem<String>(
+                value: 'submenu1',
+                child: SubmenuButton(
+                  menuStyle: MenuStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.zero),
+                  ),
+                  child: Icon(Icons.menu),
+                  menuChildren: [
+                    MenuItemButton(
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.star, size: 20),
+                          SizedBox(width: 8),
+                          Text('Option 1.1'),
+                        ],
+                      ),
+                      onPressed: () {
+                        // Handle Option 1.1
+                      },
+                    ),
+                    MenuItemButton(
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.star, size: 20),
+                          SizedBox(width: 8),
+                          Text('Option 1.2'),
+                        ],
+                      ),
+                      onPressed: () {
+                        // Handle Option 1.2
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 8), // Add some spacing between the buttons
+              DropdownMenuItem<String>(
+                value: 'submenu2',
+                child: SubmenuButton(
+                  menuStyle: MenuStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.zero),
+                  ),
+                  child: Icon(Icons.menu),
+                  menuChildren: [
+                    // ... (similar modifications for submenu2)
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
